da6f24dfe44be5813707cfb3237845b1ee040b28
TshakalisaKhupe <tkhupe@gmail.com>
2021-10-25 00:35:27 -0500

    asdasdasdasd

--- chooseyEnd.js
+++ chooseyEnd.js
@@ -1,10 +1,10 @@
-// Your code here
+// Your code here asdasd
 
 
-console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'ly'));
+console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'ly'));
 // [ 'family', 'fly', 'timidly' ]
 
-console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'nd'));
+console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'nd'));
 // [ 'hound', 'bond' ]
 
 console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'icity'));
