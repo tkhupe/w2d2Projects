127484dd0bbb78a09b037b49955744c5a8ed0d53
tshakalisa <tkhupe@gmail.com>
2021-10-30 01:18:23 -0500

    job

---
+++ POJO-W2D2 (1)/POJO-W2D2/problems/08-value-pair.js
@@ -0,0 +1,28 @@
+/***********************************************************************
+Write a function `valuePair(obj1, obj2, key)` that takes in two objects
+and a key (string). The function should return an array containing the
+corresponding values of the objects for the given key.
+
+Examples:
+let object1 = {name: 'One', location: 'NY', age: 3};
+let object2 = {name: 'Two', location: 'SF'};
+valuePair(object1, object2, 'location'); // => [ 'NY', 'SF' ]
+valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
+***********************************************************************/
+
+function valuePair(obj1, obj2, key) {
+  let val1 = obj1[key];
+  let val2 = obj2[key];
+  let arr = [val1, val2];
+
+  return arr;
+}
+
+
+let object1 = {name: 'One', location: 'NY', age: 3};
+let object2 = {name: 'Two', location: 'SF'};
+console.log(valuePair(object1, object2, 'location')); // => [ 'NY', 'SF' ]
+console.log(valuePair(object1, object2, 'name')); // => [ 'One', 'Two' ]
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = valuePair;
